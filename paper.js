fffb979b136f01e7e7180b2598ec418df86a0258
Iurie Turcan <iutscan1989@gmail.com>
2021-05-07 03:01:37 +0300

    Game Project

---
+++ paper.js
@@ -0,0 +1,61 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' ) {
+    return userInput;
+  } else {
+    console.log('Error!');
+  }
+}
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+    return 'rock';
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissors';
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if(userChoice === computerChoice) {
+    return 'Game was a tie!';
+  }
+  if(userChoice === 'rock') {
+    if(computerChoice === 'paper'){
+    return 'The computer won!';
+  } else {
+    return 'You won!';
+  }
+}
+
+if(userChoice === 'paper') {
+  if(computerChoice === 'scissors') {
+  return 'Computer Won!';
+    } else {
+      return 'You Won!';
+    }
+  }
+
+if(userChoice === 'scissors') {
+  if(computerChoice === 'Rock') {
+  return 'Computer Won!';
+    } else {
+      return 'You Won!';
+    }
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice('rock');
+  const computerChoice = getComputerChoice();
+  console.log(`You threw ${userChoice}`);
+  console.log(`Computer Threw ${computerChoice}`);
+
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame()
+
